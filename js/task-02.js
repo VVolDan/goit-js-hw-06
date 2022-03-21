77afd72d25c8c8cf7f38df7acecbb2cfa04fa1d0
VVolDan <vdan2019@gmail.com>
2022-03-21 14:36:34 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+ingredients.forEach(ingredient => {
+
+  const liElement = document.createElement('li');
+  liElement.textContent = ingredient;
+  liElement.classList.add('item');
+
+  const list = document.querySelector('#ingredients')
+  list.append(liElement);
+});
+
+
+
