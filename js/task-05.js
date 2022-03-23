405369b2e9f9b125cf1740beb07f7372675e6a6d
VVolDan <vdan2019@gmail.com>
2022-03-23 20:49:09 +0200

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
+
+input.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+    const name = event.currentTarget.value;
+    if (name)
+    output.textContent = name;
+};
