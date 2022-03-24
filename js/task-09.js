7628038a502d536484293123e3aaa460333d5ac7
VVolDan <vdan2019@gmail.com>
2022-03-24 08:56:22 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const body = document.body;
+const buttonChangeColor = document.querySelector('button.change-color');
+const colorValue = document.querySelector('span.color');
+
+buttonChangeColor.addEventListener('click', onButtonChangeColor);
+
+function onButtonChangeColor(event) {
+  const randomColor = getRandomHexColor(event);
+
+  colorValue.textContent = randomColor;
+  body.style.backgroundColor = randomColor;
 }
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+
