396430cdcbbdc14810541ca3b3ce7de2bdd35458
VVolDan <vdan2019@gmail.com>
2022-03-26 18:19:36 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,52 @@
+const refs = {
+  amountOfBoxes: document.querySelector('input'),
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+  areaOfBoxes: document.querySelector('div#boxes'),
+};
+
+
+// Creation of boxes;
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
+let amount;
+
+refs.amountOfBoxes.addEventListener('input', onFormInput);
+
+function onFormInput(event) {
+  amount = event.currentTarget.value;
+  return amount;
+};
+
+function createBoxes(amount) {
+  const boxes = [];
+  for (let i = 0; i < amount; i++) {
+    const box = `<div style="background-color: ${getRandomHexColor()}; width:${(30 + 10*i)}px; height:${(30 + 10*i)}px"></div>`;
+    boxes.push(box);
+  }  
+  return boxes.join('');
+};
+
+function onCreateBtn(event) {
+  refs.areaOfBoxes.innerHTML= createBoxes(amount);
 }
+
+refs.createBtn.addEventListener('click', onCreateBtn);
+
+// Destroing of boxes;
+
+refs.destroyBtn.addEventListener('click', destroyBoxes)
+
+function destroyBoxes() {
+  refs.areaOfBoxes.innerHTML = "";
+  refs.amountOfBoxes.value = "";
+  amount = 0;
+}
+
+const number = document.querySelector('input').value;
+console.log(number);
